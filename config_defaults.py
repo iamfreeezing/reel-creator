027b711422fc10209503ab60db6262dd17250b83
Nishant Singh <119221409+iamfreeezing@users.noreply.github.com>
2024-02-11 00:46:46 +0530

    Create config_defaults.py

---
+++ config_defaults.py
@@ -0,0 +1,4 @@
+import os
+FFMPEG_BINARY = os.getenv('FFMPEG_BINARY', 'ffmpeg-imageio')
+#IMAGEMAGICK_BINARY = os.getenv('IMAGEMAGICK_BINARY', 'auto-detect')
+IMAGEMAGICK_BINARY = "C:\\Program Files\\ImageMagick-7.1.1-Q16-HDRI\\magick.exe"
