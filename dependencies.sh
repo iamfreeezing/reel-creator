f08b8482447f558cb218d379283b2d19fff87469
Nishant Singh <119221409+iamfreeezing@users.noreply.github.com>
2024-02-11 00:46:42 +0530

    Create dependencies.sh

---
+++ dependencies.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define required dependencies
+dependencies=(
+    g4f
+    typing
+    termcolor
+    moviepy
+    firebase-admin
+    google-auth-oauthlib
+    pexels-api-py
+    requests
+    srt_equalizer
+    pydub
+    tk
+    customtkinter
+    # Add more packages as needed
+)
+
+# Install dependencies using pip
+for package in "${dependencies[@]}"; do
+    pip install "$package"
+done
